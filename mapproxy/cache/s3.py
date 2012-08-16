fc85c978f5d32130bdb7dbb8140026189999b3b3
Matt Walker <walkermatt@longwayaround.org.uk>
2012-08-16 20:58:44 +0100

    Tidy up to remove unused imports and old cruft

--- mapproxy/cache/s3.py
+++ mapproxy/cache/s3.py
@@ -2,9 +2,8 @@ from __future__ import with_statement
 import os
 import errno
 
-from mapproxy.util import ensure_directory
-from mapproxy.image import ImageSource, is_single_color_image
-from mapproxy.cache.base import TileCacheBase, FileBasedLocking, tile_buffer
+from mapproxy.image import ImageSource
+from mapproxy.cache.base import tile_buffer
 from mapproxy.cache.file import FileCache
 
 import boto
@@ -27,43 +26,31 @@ class S3Cache(FileCache):
             each tile (e.g. 'png')
         """
         super(S3Cache, self).__init__(cache_dir, file_ext=file_ext, directory_layout=directory_layout, lock_timeout=lock_timeout, link_single_color_images=False)
-        self.bucket_id = bucket
+
         self.s3_conn = boto.connect_s3()
-        # b = self.s3_conn.create_bucket(self.bucket)
 
         try:
-            self.bucket = self.s3_conn.get_bucket(self.bucket_id)
+            self.bucket = self.s3_conn.get_bucket(bucket)
         except boto.exception.S3ResponseError, e:
             if e.error_code == 'NoSuchBucket':
-                self.bucket = self.s3_conn.create_bucket(self.bucket_id, location=boto.s3.connection.Location.EU)
-
-        log.info('bucket: %s' % self.bucket)
+                self.bucket = self.s3_conn.create_bucket(bucket, location=boto.s3.connection.Location.EU)
 
     def load_tile_metadata(self, tile):
-        location = self.tile_location(tile)
-        try:
-            stats = os.lstat(location)
-            tile.timestamp = stats.st_mtime
-            tile.size = stats.st_size
-        except OSError, ex:
-            if ex.errno != errno.ENOENT: raise
-            tile.timestamp = 0
-            tile.size = 0
-    
+        # TODO Implement storing / retrieving tile metadata
+        tile.timestamp = 0
+        tile.size = 0
+
     def is_cached(self, tile):
         """
         Returns ``True`` if the tile data is present.
         """
-        # return False
-        log.info('missing: %s' % tile.is_missing())
         if tile.is_missing():
+
             location = self.tile_location(tile)
-            # conn = boto.connect_s3()
-            # b = self.s3_conn.get_bucket(self.bucket)
+            log.debug('is_cached, location: %s' % location)
+
             k = boto.s3.key.Key(self.bucket)
-            log.info('is_cached, location: %s' % location)
             k.key = location
-            log.info('exists: %s' % k.exists())
             if k.exists():
                 return True
             else:
@@ -80,17 +67,13 @@ class S3Cache(FileCache):
             return True
 
         location = self.tile_location(tile)
+        log.debug('load_tile, location: %s' % location)
 
-        # conn = boto.connect_s3()
         tile_data = StringIO.StringIO()
-        # b = self.s3_conn.get_bucket(self.bucket)
         k = boto.s3.key.Key(self.bucket)
-        log.info('load_tile, location: %s' % location)
         k.key = location
-        # if k.exists():
         try:
             k.get_contents_to_file(tile_data)
-            # k.get_contents_to_filename('/home/matt/Projects/MapProxy/tile.png')
             tile.source = ImageSource(tile_data)
             k.close()
             return True
@@ -99,20 +82,11 @@ class S3Cache(FileCache):
         k.close()
         return False
 
-        # if os.path.exists(location):
-        #     if with_metadata:
-        #         self.load_tile_metadata(tile)
-        #     tile.source = ImageSource(location)
-        #     return True
-        # return False
-
     def remove_tile(self, tile):
+
         location = self.tile_location(tile)
-        log.info('remove_tile, location: %s' % location)
-        # try:
-        #     os.remove(location)
-        # except OSError, ex:
-        #     if ex.errno != errno.ENOENT: raise
+        log.debug('remove_tile, location: %s' % location)
+
         k = boto.s3.key.Key(self.bucket)
         k.key = location
         if k.exists():
@@ -128,26 +102,25 @@ class S3Cache(FileCache):
             return
 
         location = self.tile_location(tile)
+        log.debug('store_tile, location: %s' % location)
 
-        log.info('store_tile, location: %s' % location)
-        # conn = boto.connect_s3()
-        # b = self.s3_conn.get_bucket(self.bucket)
         k = boto.s3.key.Key(self.bucket)
         k.key = location
         with tile_buffer(tile) as buf:
             k.set_contents_from_file(buf)
         k.close()
 
+        # Attempt making storing tiles non-blocking
+
         # This is still blocking when I thought that it would not
         # async.run_non_blocking(self.async_store, (k, tile))
 
         # async_pool = async.Pool(4)
         # for store in async_pool.map(self.async_store_, [(k, tile)]):
-        #     log.info('stored...')
-
-        # async.starmap(self.async_store, (k, tile))
+        #     log.debug('stored...')
 
         # This sometimes suffers from "ValueError: I/O operation on closed file"
+        # as I guess it's not advised to use threads within a wsgi app
         # Timer(0.25, self.async_store, args=[k, tile]).start()
 
     def async_store_(self, foo):
